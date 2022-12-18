bdb4620859226fec0259f4532f60366295d21065
Oleg Terentev <terentjev.dr@yandex.ru>
2022-12-19 01:28:28 +0300

    Project for Tomcat and Jenkins

--- src/main/java/HelloAston.java
+++ src/main/java/HelloAston.java
@@ -1,18 +1,19 @@
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/hello")
+@WebServlet("/hello")
 public class HelloAston extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("Hello, Aston!");
+        out.println("<h2>Hello, Aston!</h2>");
         out.close();
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello, bro!</h2>
 </body>
 </html>
